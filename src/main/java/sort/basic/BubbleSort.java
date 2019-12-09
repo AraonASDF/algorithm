4a8b1613d515d5c8474a987d19968295c89d7685
电科信链杨家兴 <qc_00ba19bc7cec4c7b9e3100e8788be7dc@git.code.tencent.com>
2019-12-09 14:18:25 +0800

    init commit

---
+++ src/main/java/sort/basic/BubbleSort.java
@@ -0,0 +1,25 @@
+package sort.basic;
+
+/**
+ * @author yjx
+ * @date 2019/12/9
+ **/
+public class BubbleSort {
+
+    public static void sort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swap(arr, j, j + 1);
+                }
+            }
+        }
+    }
+
+    public static void swap(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+
+}
